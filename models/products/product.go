8c60bdefaab5f76eb5df0d5db22be932164c7994
csailer <chuck@chucksailer.com>
2023-04-02 13:24:16 -0400

    add flags to Product struct ctor()

--- models/products/product.go
+++ models/products/product.go
@@ -12,31 +12,37 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type GTIN struct {
+	Type  string `bson:"type,omitempty" json:"type"`
+	Value string `bson:"value,omitempty" json:"value"`
+}
+
 type Product struct {
-	ID                   primitive.ObjectID   `bson:"_id,omitempty"`
-	URL                  string               `bson:"url"`
-	CanonicalURL         string               `bson:"canonicalUrl"`
-	Probability          float64              `bson:"probability"`
-	Name                 string               `bson:"name"`
-	Offers               []models.Offer              `bson:"offers"`
-	SKU                  string               `bson:"sku"`
-	MPN                  string               `bson:"mpn"`
-	Brand                string               `bson:"brand"`
-	Breadcrumbs          []models.Breadcrumb         `bson:"breadcrumbs"`
-	MainImage            string               `bson:"mainImage"`
-	Images               []string             `bson:"images"`
-	Description          string               `bson:"description"`
-	DescriptionHTML      string               `bson:"descriptionHtml"`
-	AdditionalProperties []models.AdditionalProperty `bson:"additionalProperty"`
-	AggregateRating      models.AggregateRating      `bson:"aggregateRating"`
-	Color                string               `bson:"color"`
-	Style                string               `bson:"style"`
-	DateCreated          models.CustomTime           `bson:"dateCreated"`
-	DateModified         models.CustomTime           `bson:"dateModified"`
-	isNew                bool                 `bson:"isNew"`
-	isDeleted            bool                 `bson:"isDeleted"`
-	isDirty              bool                 `bson:"isDirty"`
-	originalState        *Product             `bson:"-"`
+	ID                   primitive.ObjectID   `bson:"_id,omitempty" json:"_id,omitempty"`
+	URL                  string               `bson:"url" json:"url"`
+	CanonicalURL         string               `bson:"canonicalUrl" json:"canonicalUrl"`
+	Probability          float64              `bson:"probability" json:"probability"`
+	Name                 string               `bson:"name" json:"name"`
+	Offers               []models.Offer       `bson:"offers" json:"offers"`
+	SKU                  string               `bson:"sku" json:"sku"`
+	MPN                  string               `bson:"mpn,omitempty" json:"mpn,omitempty"`
+	Brand                string               `bson:"brand,omitempty" json:"brand,omitempty"`
+	Breadcrumbs          []models.Breadcrumb  `bson:"breadcrumbs" json:"breadcrumbs"`
+	MainImage            string               `bson:"mainImage" json:"mainImage"`
+	Images               []string             `bson:"images" json:"images"`
+	Description          string               `bson:"description" json:"description"`
+	DescriptionHTML      string               `bson:"descriptionHtml" json:"descriptionHtml"`
+	AdditionalProperties []models.AdditionalProperty `bson:"additionalProperty" json:"additionalProperty"`
+	AggregateRating      models.AggregateRating      `bson:"aggregateRating" json:"aggregateRating"`
+	GTINs                []GTIN               `bson:"gtins,omitempty" json:"gtin,omitempty"`
+	Color                string               `bson:"color,omitempty" json:"color,omitempty"`
+	Style                string               `bson:"style,omitempty" json:"style,omitempty"`
+	DateCreated          models.CustomTime           `bson:"dateCreated,omitempty" json:"dateCreated,omitempty"`
+	DateModified         models.CustomTime           `bson:"dateModified,omitempty" json:"dateModified,omitempty"`
+	isNew                bool                 `bson:"isNew,omitempty" json:"isNew,omitempty"`
+	isDeleted            bool                 `bson:"isDeleted,omitempty" json:"isDeleted,omitempty"`
+	isDirty              bool                 `bson:"isDirty,omitempty" json:"isDirty,omitempty"`
+	originalState        *Product             `bson:"-" json:"-"`
 }
 
 
@@ -58,6 +64,9 @@ func New(options ...func(*Product)) *Product {
 	for _, option := range options {
 		option(product)
 	}
+	product.isNew = true
+	product.isDeleted = false
+	product.isDirty = false
 	return product
 }
 
