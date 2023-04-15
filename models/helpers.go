51f99d5976754c9948a4f50a1d69992ad1441aa2
csailer <chuck@chucksailer.com>
2023-04-15 19:48:38 -0400

    Release Version: v1.2.9

--- models/helpers.go
+++ models/helpers.go
@@ -36,7 +36,7 @@ func ExtractCompanyName(urlString string) (string, error) {
 }
 
 // Categorizes all products which are not already categorized
-func Categorize() error {
+func Categorize(cfg config.BizObjConfig) error {
 	// - Get all products that are not categorized
 	prd := NewProduct(
 		NewProductWithBizObjConfig(*_cfg),
