b82253ec920e9cc720e95d14842a563b37fb03d0
Chuck Sailer <csailer@acadian-asset.com>
2023-04-27 08:56:10 -0400

    Release Version: v1.2.53

--- models/product.go
+++ models/product.go
@@ -168,9 +168,9 @@ func (p *Product) Save() error {
 		p.IsCategorized = false
 		// Mark product as processed
 		p.FilesProcessed = true
-		
+
 		//-- Upsert document
-		err = mongoDB.Upsert(p, "canonicalURL")
+		err = mongoDB.Upsert(p, "canonicalUrl")
 		if err != nil {
 			logging.Error("Product.Save() Error creating/updating Product in MongoDB: %s", err.Error())
 			return errors.NewChuxModelsError("Product.Save() Error creating/updating Product in MongoDB", err)
